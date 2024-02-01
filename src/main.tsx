0821e1c5baf7d5243b98d2137266d934b7c10b4e
ShaqeTarverdyan <tshaqe@gmail.com>
2024-02-01 17:17:59 +0400

    Complete plugin

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { 
+  initializePositions, 
+  setPosition, 
+  updateActivePositions, 
+  store 
+} from './store';
+import { PluginOptions } from './types';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+
+declare global {
+  interface Window {
+    MY_APP: any,
+    DAMAGE_SELECTOR_API: any
+  }
+}
+
+
+if (import.meta.env.MODE === 'development') {
+  const renderElement = document.getElementById('root');
+  ReactDOM.createRoot(renderElement as HTMLElement).render(
+    <React.StrictMode>
+      <App
+        options={{
+          initializedOptions: store.getState(),
+          onInit: initializePositions,
+          onComplete: (positions: string[]) => setPosition(positions),
+          onPositionChange: (positions: string[]) => updateActivePositions(positions),
+        }}
+      />
+    </React.StrictMode>
+  );
+} else {
+  window.DAMAGE_SELECTOR_API = {
+    init: (options: PluginOptions) => {
+      const { selector } = options;
+      if (selector) {
+        const renderElement = document.querySelector(selector);
+        if (renderElement) {
+          ReactDOM.createRoot(renderElement as HTMLElement).render(
+            <React.StrictMode>
+              <App options={{
+                selector: selector,
+                initializedOptions: store.getState(),
+                onInit: initializePositions,
+                onComplete: (positions: string[]) => setPosition(positions),
+                onPositionChange: (positions: string[]) => updateActivePositions(positions),
+              }} />
+            </React.StrictMode>
+          );
+        }
+      }
+    },
+  };
+}
