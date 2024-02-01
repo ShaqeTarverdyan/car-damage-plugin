05d5e50504440aec244377bf76f40dd8848f2539
ShaqeTarverdyan <tshaqe@gmail.com>
2024-02-01 17:17:19 +0400

    add initial data for circles

---
+++ src/initialData.ts
@@ -0,0 +1,56 @@
+import { CircleInterface } from './types';
+
+export const carPositions: CircleInterface[] =
+[ {
+     id: 1,
+     name: 'A1',
+ },
+ {
+     id: 2,
+     name: 'A2',
+ },
+ {
+     id: 3,
+     name: 'A3',
+ },
+ {
+     id: 4,
+     name: 'A4',
+ },
+ {
+     id: 5,
+     name: 'B1',
+ },
+ {
+     id: 6,
+     name: 'B2',
+ },
+ {
+     id: 7,
+     name: 'B3',
+ },
+ {
+     id: 8,
+     name: 'B4',
+ },
+ {
+     id: 9,
+     name: 'B5',
+ },
+ {
+     id: 10,
+     name: 'C1',
+ },
+ {
+     id: 11,
+     name: 'C2',
+ },
+ {
+     id: 12,
+     name: 'C3',
+ },
+ {
+     id: 13,
+     name: 'C4',
+ }
+]
